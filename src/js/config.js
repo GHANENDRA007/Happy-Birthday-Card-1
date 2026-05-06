3f460e564281d54288e9efeefda55777407530c1
GHANENDRA007 <ghanendrabvbvpn0131@gmail.com>
2026-05-06 16:29:29 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,11 +1,19 @@
 export default {
-  name: process.env.NAME, // actual name of the recipient (Mandatory)
+  name: ISABELLE, // actual name of the recipient (Mandatory)
 
   nickname: process.env.NICKNAME, // nickname(optional)
 
   pic: process.env.PIC, // image url of recipients (Mandatory)
 
-  showScrollMsg: process.env.SCROLL_MSG, // set to false if you do not want the scrolling message
+  showScrollMsg: Happy 19th Birthday to one of the most special people in my life ❤️
 
-  birthDate: process.env.BIRTH_DATE, // Mention birthday date in YYYY-MM-DD format
+Even though miles separate us, you’ve become such an important part of my world. You’re not just my long-distance best friend, you’re someone I truly cherish deeply. Thank you for always being there, for your kindness, your laughter, your endless support, and for making even the farthest distance feel so small.
+
+You mean more to me than words can fully express, and I’m so grateful for every conversation, every memory, and every moment we share. On your special day, I just want you to know how incredibly loved, valued, and appreciated you are.
+
+May this year bring you endless happiness, success, beautiful memories, and everything your heart desires because you truly deserve the absolute best.
+
+Keep shining, beautiful soul. Happy Birthday once again ❤️🎉, // set to false if you do not want the scrolling message
+
+  birthDate: 2026-05-06, // Mention birthday date in YYYY-MM-DD format
 };
