a70d49dbcc7f81588e7ee59ff8746bd6764854b6
GHANENDRA007 <ghanendrabvbvpn0131@gmail.com>
2026-05-06 16:47:29 +0530

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,7 @@ export default {
 
   nickname: process.env.NICKNAME, // nickname(optional)
 
-  pic: process.env.PIC, // image url of recipients (Mandatory)
+  pic: https://media.discordapp.net/attachments/1204725680978526208/1501543166329749574/Screenshot_20260506_164447_Instagram.jpg?ex=69fc748c&is=69fb230c&hm=acbd1656693b95b60543430fb73d61dc768e93920760da3cae3622c4061848ab&=&format=webp&width=418&height=525, // image url of recipients (Mandatory)
 
   showScrollMsg: process.env.SCROLL_MSG, // set to false if you do not want the scrolling message
 
